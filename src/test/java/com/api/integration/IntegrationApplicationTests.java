2812ee825b68f38a1a3f7bf9c4c30b885c018b6e
fecmartins <85011293+fecmartins@users.noreply.github.com>
2024-10-13 18:22:37 +0100

    Initial commit

---
+++ src/test/java/com/api/integration/IntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
